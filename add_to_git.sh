37f051d8a885190efcc00b05258fc06611e5f131
Keterion <hightech-nik@web.de>
2024-02-26 15:20:29 +0100

    initial commit

---
+++ add_to_git.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ingore=$(cat .gitignore | grep -oP "(?<=!)[^/]+") # gets all the whitelisted things in .gitignore
+index=0
+possible=()
+for f in *; do # all files in .config
+  if ! [[ $ingore =~ $f ]]; then # if it isn't whitelisted already
+    echo $index:$f;
+    ((index++));
+    possible+=( "$f" );
+  fi
+done
+
+indicies="USER INPUT"
+read -p "Select indicies to whitelist: " indicies
+for i in $indicies; do # appends the files/directories to .gitignore
+  echo adding ${possible[$i]} to .gitignore
+  if [[ -d ${possible[$i]} ]]; then
+    echo "!${possible[$i]}/" >> .gitignore
+  else
+    echo "!${possible[$i]}" >> .gitignore
+  fi
+done
